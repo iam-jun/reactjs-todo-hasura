f368f02fde56954e276c06d37d4f61833eaa8cd2
Mai Văn Tuấn <40934221+mvtuan@users.noreply.github.com>
2024-08-19 15:34:07 +0700

    🚀 Implement insert/update Task - Install mui, react-hook-form libraries - Implement modal for creating/updating task

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,22 +1,22 @@
-import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const httpLink = new HttpLink({
-  uri: 'https://inspired-stinkbug-15.hasura.app/v1/graphql',
+  uri: "https://inspired-stinkbug-15.hasura.app/v1/graphql",
   headers: {
     "x-hasura-admin-secret": `WQ5swuNVKiRPxvuwoqohSoEVasI0640KlZdilCSxUvP61KMw1CNcVFSQWVttr90P`,
   },
 });
 
 const wsLink = new WebSocketLink({
-  uri: 'https://inspired-stinkbug-15.hasura.app/v1/graphql',
+  uri: "wss://inspired-stinkbug-15.hasura.app/v1/graphql",
   options: {
     reconnect: true,
     connectionParams: {
       headers: {
         "x-hasura-admin-secret": `WQ5swuNVKiRPxvuwoqohSoEVasI0640KlZdilCSxUvP61KMw1CNcVFSQWVttr90P`,
-    },
+      },
     },
   },
 });
@@ -25,8 +25,8 @@ const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
---
+++ src/interfaces/Task.ts
@@ -0,0 +1,7 @@
+export interface ITask {
+  id?: string;
+  title: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
